bd593d0fb85836a412980a3d6ec78e4bddb80c54
koopa1338 <sinner1991@gmail.com>
2022-03-11 02:29:35 +0100

    WIP: brainstorming about table structure

--- egui_extras/src/table.rs
+++ egui_extras/src/table.rs
@@ -1,86 +1,69 @@
 #![allow(unused_variables, dead_code)]
 
-use egui::{Response, Ui, Vec2, Widget, WidgetText};
+/* TODO: Rethink the data structure here. Just a brainstorming: We need as a root a `Table` struct
+* that has all settings and data, to display the container correctly or settings that should be
+* global and passed down to childs.
+*
+* A table in the end is just a container, that displays a dynamic
+* number of `TableRow`s. These rows need all the settings and data to display a row and its childs
+* correctly. Each `TableRow` has a fixed numbers of `TableCell`s that display the data or Widget.
+* The `TableRow` needs also a kind of struct (a vec or slice for example) to handle multi col cells
+*
+* Table:
+*   - striped
+*   - num cols
+*   - heading
+*   - vertical alignment
+*   - future functionality:
+*       - sortable
+*       - resizable
+*       - fixed cols
+*       - paging
+*       - max results per page
+*
+* TableRow:
+*   - horizontal align
+*   - collapsible
+*
+* TableCell:
+*   - editable
+*   - mark
+*   - copyable
+*
+*/
 
-/// WIP: table widget
-///
-#[must_use = "You should put this widget in an ui with `ui.add(widget);`"]
+enum VertAlignment {
+    Left,
+    Center,
+    Right,
+}
+
+enum HorizAlignment {
+    Top,
+    Center,
+    Bottom,
+}
+
 #[derive(Default)]
 pub struct Table {
     num_columns: usize,
-    fixed_cols: Option<Vec<usize>>,
-    resizable: bool,
-    sortable_cols: Option<Vec<usize>>,
-    title: Option<WidgetText>,
     striped: bool,
-    min_col_width: Option<f32>,
-    min_row_height: Option<f32>,
-    max_cell_size: Vec2,
-    spacing: Vec2,
-    searchable: bool,
+    display_headers: bool,
+    col_alignment: Vec<VertAlignment>,
+
+    /* future functionality: resizable: bool,
+    * sortable_cols: Vec<usize>,
+    * searchable: bool,
+    * paging: bool,
+    * max_results_per_page: usize,
+    */
 }
 
-impl Table {
-    pub fn new(columns: usize) -> Self {
-        Self {
-            num_columns: columns,
-            ..Default::default()
-        }
-    }
-
-    pub fn resizable(mut self, resizable: bool) -> Self {
-        self.resizable = resizable;
-        self
-    }
-
-    pub fn title(mut self, title: impl Into<WidgetText>) -> Self {
-        self.title = Some(title.into());
-        self
-    }
-
-    pub fn striped(mut self, striped: bool) -> Self {
-        self.striped = striped;
-        self
-    }
-
-    pub fn searchable(mut self, searchable: bool) -> Self {
-        self.searchable = searchable;
-        self
-    }
-
-    pub fn fixed_cols(mut self, fixed_cols: impl Into<Vec<usize>>) -> Self {
-        self.fixed_cols = Some(fixed_cols.into());
-        self
-    }
-
-    pub fn sortable_cols(mut self, sortable_cols: impl Into<Vec<usize>>) -> Self {
-        self.sortable_cols = Some(sortable_cols.into());
-        self
-    }
-
-    pub fn spacing(mut self, spacing: impl Into<Vec2>) -> Self {
-        self.spacing = spacing.into();
-        self
-    }
-
-    pub fn max_cell_size(mut self, max_cell_size: impl Into<Vec2>) -> Self {
-        self.max_cell_size = max_cell_size.into();
-        self
-    }
-
-    pub fn min_col_width(mut self, min_col_width: f32) -> Self {
-        self.min_col_width = Some(min_col_width);
-        self
-    }
-
-    pub fn min_row_height(mut self, min_row_height: f32) -> Self {
-        self.min_row_height = Some(min_row_height);
-        self
-    }
+pub struct TableRow {
+    cell_alignment: Vec<HorizAlignment>,
+    collapsible: bool,
 }
 
-impl Widget for Table {
-    fn ui(self, ui: &mut Ui) -> Response {
-        todo!()
-    }
+pub struct TableCell {
+    editable: bool,
 }
